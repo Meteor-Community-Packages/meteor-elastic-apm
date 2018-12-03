db62753f3f8ee3defe651264cb2b0542b0a5aa97
vinc <vinc@vinc-imac.42.fr>
2018-12-03 15:18:31 +0100

    change db.js to avoid underscore dependency and don't import meteor-elastic-apm on the client side

--- hijack/db.js
+++ hijack/db.js
@@ -11,7 +11,7 @@ function start(apm){
     var self = this;
     var ret = originalOpen.call(self, name);
 
-    _.each(ret, function(fn, m) {
+    Object.keys(ret).forEach(ret, function(fn, m) {
       // make sure, it's in the actual mongo connection object
       // meteorhacks:mongo-collection-utils package add some arbitary methods
       // which does not exist in the mongo connection
