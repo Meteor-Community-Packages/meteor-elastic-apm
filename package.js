d854b8405a3a6eb1a6db68dab454ba686ddf3103
Shynggys <kschingiz@gmail.com>
2019-01-18 17:35:23 +0600

    Package version update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "kschingiz:meteor-elastic-apm",
-  version: "0.0.5",
+  version: "1.0.0",
   // Brief, one-line summary of the package.
   summary: "Performance monitoring for Meteor based on Elastic APM",
   // URL to the Git repository containing the source code for this package.
