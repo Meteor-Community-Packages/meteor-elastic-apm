da4dc0c4ad4d019765aea325639e1dafc4eb658a
Kengessov <kschingiz@gmail.com>
2019-07-19 19:54:10 +0600

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 Package.describe({
   name: 'kschingiz:meteor-elastic-apm',
-  version: '2.2.0',
+  version: '2.2.1',
   // Brief, one-line summary of the package.
   summary: 'Performance monitoring for Meteor based on Elastic APM',
   // URL to the Git repository containing the source code for this package.
@@ -45,4 +45,3 @@ Package.onTest(function(api) {
   api.use('kschingiz:meteor-elastic-apm');
   api.mainModule('meteor-elastic-apm-tests.js');
 });
-
