b5be13e8d8f7a076f453f6d991bde309f7806c00
Simon Schick <SimonSimCity@gmail.com>
2019-08-06 11:47:52 +0200

    Set route and method as transaction name if possible

--- instrumenting/http-in.js
+++ instrumenting/http-in.js
@@ -6,6 +6,19 @@ function start(agent, WebApp) {
     const span = agent.startSpan(EXECUTION);
 
     res.on('finish', () => {
+      let route = req.originalUrl;
+      if (req.originalUrl.endsWith(req.url.slice(1)) && req.url.length > 1) {
+        route = req.originalUrl.slice(0, -1 * (req.url.length - 1));
+      }
+  
+      if (route.endsWith('/')) {
+        route = route.slice(0, -1);
+      }
+  
+      if (route) {
+        transaction.name = `${req.method}:${route}`;
+      }
+
       span.end();
       transaction.__span = agent.startSpan(SENDING);
     });
